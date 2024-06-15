27a11b4fda373a1e247dbeb3fbd304cca84aa6b3
bernard411 <hamiebernard@gmail.com>
2024-06-15 11:09:16 -0500

    up

--- core/urls.py
+++ core/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('welcome', views.home, name='home'),
     path('ai_doctor/', views.ai_doctor, name='ai_doctor'),
     path('health_records/', views.health_records, name='health_records'),
     path('geo_mapping/', views.geo_mapping, name='geo_mapping'),
     path('medicine/', views.medicine, name='medicine'),
-    path('authanticate/', views.login_view, name='login'),
+    path('', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
 ]

2a4bc87a99034e0e6cd43b643710a8c18b433a38
bernard411 <hamiebernard@gmail.com>
2024-06-17 19:04:03 -0500

    jj

--- core/views.py
+++ core/views.py
@@ -71,7 +71,7 @@ def home(request):
         'records': records,
         'recods_count': recods_count
     }
-    return render(request, 'home.html', context)
+    return render(request, 'index.html', context)
 
 def ai_doctor(request):
     return render(request, 'doc.html')
